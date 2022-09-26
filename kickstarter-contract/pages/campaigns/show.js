601c8462602020fd5f321ab1e9161a65a1681a16
Cuong Xuan Do <xuancuong.do@r4pid.com>
2022-09-26 16:49:07 +0700

    update

--- kickstarter-contract/pages/campaigns/show.js
+++ kickstarter-contract/pages/campaigns/show.js
@@ -1,14 +1,18 @@
 import React , { Component } from 'react'
 import Layout from '../../components/Layout'
 import Campaign from '../../ethereum/campaign'
-import { Card } from 'semantic-ui-react'
+import { Card , Grid  , Button} from 'semantic-ui-react'
 import web3 from '../../ethereum/web3'
+import ContributeForm from '../../components/ContributeForm'
+import { Link } from '../../routes'
+
 class  CampaignShow extends Component {
      static async getInitialProps(props) {
           const campaign = Campaign(props.query.address)
           const summary = await campaign.methods.getSummary().call()
           console.log(summary)
           return {
+               address: props.query.address,
                minimumContribution : summary[0],
                balance : summary[1],
                requestsCount : summary[2],
@@ -67,7 +71,32 @@ class  CampaignShow extends Component {
           return(
                <Layout> 
           <h3>Campaign show</h3>
-          {this.recentCards()}
+
+<Grid>
+     <Grid.Row>
+     <Grid.Column width={10}>
+     {this.recentCards()}
+    
+     </Grid.Column>
+
+     <Grid.Column width={6}>
+     <ContributeForm address={this.props.address} />
+     </Grid.Column>
+     </Grid.Row>
+     <Grid.Row>
+          <Grid.Column>
+     <Link route={`/campaigns/${this.props.address}/requests` }>
+          <a>
+          <Button primary>View Requests</Button>
+          </a>
+     </Link>
+     </Grid.Column>
+     </Grid.Row>
+
+</Grid>
+
+   
+          
                </Layout>
           )
      }
